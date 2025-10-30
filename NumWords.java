5b5bf9b63a0306b43f57543cf25fe84aafe5cd5f
Itay Reznik <itay.reznik@post.runi.ac.il>
2025-10-30 18:57:44 +0200

    Done

--- NumWords.java
+++ NumWords.java
@@ -9,20 +9,5 @@ public class NumWords {
 		//Print the number's value in terms of hundreds, tens, ones.
 		System.out.println((number / 100) + " hundreds, " + ((number % 100) / 10) + " tens, " + (number % 10) + " ones.");
 
-		 
-	/*Option 2 :
-		//Retrieve number
-		int number = Integer.parseInt(args[0]);
-
-		//Declairing into variables the results
-		int hundreds = number / 100;
-		int tens = (number % 100) / 10;
-		int ones = number % 10;
-
-		//Print the number's value in terms of hundreds, tens, ones.
-		System.out.println((number / 100) + " hundreds, " + ((number % 100) / 10) + " tens, " + (number % 10) + " ones.");
-
-	*/
-
 	}
 }
